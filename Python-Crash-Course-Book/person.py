a1f8709844e3578972b1f6e690899a03a360267c
jermartinz <jeremyastudillo19@outlook.com>
2022-04-21 04:03:56 -0400

    added more exercises

--- Python-Crash-Course-Book/person.py
+++ Python-Crash-Course-Book/person.py
@@ -1,27 +1,82 @@
-#6.1
-person = {
-    'first_name': 'John',
-    'last_name': 'Davis',
-    'age': 22,
-    'city': 'New York'
-}
-print(person)
+#6.1 and #6.7
 
+import numbers
+
+
+member = {'first_name': 'john', 'last_name': 'davis', 'age': 22}
+    
+astronaut = {'first_name': 'elon', 'last_name': 'hinton', 'age': 35}
+
+pilot = {'first_name': 'david', 'last_name': 'ward', 'age': 38}
+
+person = [member, astronaut, pilot]
+
+for crew_member in person[:3]:
+    print(f"\n{crew_member}")
+
+
+#6.2
 favorite_numbers = {
-    'liam': 7,
-    'amelia': 4,
-    'benjamin': 12,
-    'violet': 8,
-    'olivia': 9
-}
-print(favorite_numbers)
+    'liam': [7, 2],
 
+    'amelia':[4, 10],
+
+    'benjamin': [12, 20],
+
+    'violet': [8, 16],
+
+    'olivia': [9, 32]
+    
+}
+for people, favorite_n in favorite_numbers.items():
+    print(f"\n{people.title()}'s favorite number is:")
+    for favorite in favorite_n:
+        print(f"\t{favorite}")
+
+#6.3
 glossary = {
     'insert': 'You can add a new element at any position in your list by using the insert method',
     'pop': 'You can use pop to remove an item from any position in a list',
     't': 'To add a tab to your text, use this character combination',
     'n': 'To add a newline in a string, use this character combination',
     'tuple': 'A tuple looks just like a list except you use parentheses instead of square brackets'
+    
+}
 
-}  
-print (glossary)
+#6.4
+for word, meaning in glossary.items():
+    print (f"\n {word.title()}: {meaning}")
+
+#6.5
+rivers = {
+    'ganges': 'india',
+    'nile': 'egypt',
+    'misuri': 'usa'
+}
+
+for river, country in rivers.items():
+    print(f'\nThe {river.title()} runs trough {country.title()}')
+
+
+for river in rivers.keys():
+    print(river.title())
+
+for country in rivers.values():
+    print(country.title())
+
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'jack': 'c++',
+    'chris': None,
+    'sophia': None
+
+}
+#6.6
+for person, language in favorite_languages.items():
+    if language == None:
+        print(f'Please take the poll {person.title()}')
+    else:
+        print(f'Thanks {person.title()} for respond the poll. Your vote was {language.title()}.')
---
+++ Python-Crash-Course-Book/pizza.py
@@ -0,0 +1,94 @@
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+
+print(f"You ordered a {pizza['crust']}-crust pizza "
+    "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print(f"\t{topping}")
+
+
+#many_user.py
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    },
+}
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
+
+
+#6.8
+
+pet_one = {
+    'kind': 'dog',
+    'name': 'firulais',
+    'owner': 'jack',
+}
+
+pet_two = {
+    'kind': 'fish',
+    'name': 'nemo',
+    'owner': 'elon',
+}
+pet_three = {
+    'kind': 'cat',
+    'name': 'michi',
+    'owner': 'louis',
+}
+
+pets = [pet_one, pet_two, pet_three]
+
+for pet in pets:
+    print(f"{pet}")
+
+#6.9 
+favorite_places = {
+    'fanny': ['brazil', 'paris'],
+    'hector': ['korea', 'japan'],
+    'jeremy': ['california', 'barcelona', 'dubai'],
+}
+for friend, places in favorite_places.items():
+    print(f"\nThe favorite places of {friend.title()} is:")
+    for place in places:
+        print(f"\t{place.title()}")
+
+cities = {
+    'paris': {
+        'country': 'france',
+        'population': '2.161 millions',
+        'fact':'city of fashion',
+    },
+    'california': {
+        'country': 'united states',
+        'population': '39.967 millions',
+        'fact':'hollywood',
+    },
+    'santiago':{
+        'country': 'chile',
+        'population': '5.161 millions',
+        'fact':'los andes',
+    },
+}
+
+for city, city_info in cities.items():
+    print(f"\nCity: {city.title()}")
+    print(f"\tCountry: {city_info['country'].title()}")
+    print(f"\tPopulation: {city_info['population']}")
+    print(f"\tFact: {city_info['fact'].title()}")
+
